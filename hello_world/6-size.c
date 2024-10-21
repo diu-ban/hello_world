a16317c695465d1f38875b36b669d18a45090219
Diu <bongban2003@gmail.com>
2024-10-21 09:32:17 +0000

    HW 4-6 indentation fix

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
 	printf("Size of a char: %u byte(s)\n", sizeof(char));
 	printf("Size of an int: %u byte(s)\n", sizeof(int));
 	printf("Size of a long int: %u byte(s)\n", sizeof(long int));
