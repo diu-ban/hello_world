b86c97593ea560098acd44f45ba991298d5b7adc
Diu <bongban2003@gmail.com>
2024-10-22 03:49:43 +0000

    view 101

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints all possible different combinations of three digits.
+ *              Numbers are separated by a comma followed by a space,
+ *              and only the smallest combination is printed in ascending
+ *              order, ensuring all digits are different.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = i + 1; i < 9; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+			}
+			if (i < 7)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
