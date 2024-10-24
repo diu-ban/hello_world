a113546378194a4751b41da68dca55405283ca66
diu <bongban2003@gmail.com>
2024-10-24 06:36:10 +0000

    FNL test 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include<_putchar.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints "_putchar" followed by a new line using
+ *              the custom _putchar function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_putchar');
+
+	return (0);
+}
