dd24d314f7d360e65f5440784c059c270d824e85
diu <bongban2003@gmail.com>
2024-10-24 09:51:54 +0000

    fnl 3-5 betty fix

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints the alphabet in lowercase.
+ * print_alphabet_x10 - Prints the alphabet in lowercase.
  *
  * Description: Prints 'a' to 'z' followed by a new line
  *              using _putchar function twice.
--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -15,6 +15,6 @@ int _islower(int c)
 	{
 		return (1);
 	}
-	
+
 	return (0);
 }
