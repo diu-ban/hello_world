2f444d71160389c371211a0cbec00ecb54d91ce3
diu <bongban2003@gmail.com>
2024-10-24 09:09:17 +0000

    fnl 2 name change

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Description: Prints 'a' to 'z' followed by a new line
+ *              using _putchar function twice.
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char ch = a;
+
+	while (i < 10)
+	{
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+
+		_putchar('\n');
+		i++;
+	}    
+}
