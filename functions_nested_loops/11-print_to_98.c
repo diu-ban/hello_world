60a0a72642f218d6e1c8a7e3466a172bad1ac3fa
diu <bongban2003@gmail.com>
2024-10-24 22:50:17 +0000

    fnl 11 fix

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -6,52 +6,44 @@
  */
 void print_to_98(int n)
 {
-	while (n < 98)
+	while (n != 98)
 	{
+		int a = n;
+		
 		if (n < 0)
 		{
+			a = -n;
 			_putchar('-');
-			_putchar(n * -1 + '0');
 		}
 
-		if ((n >= 0) && (n <= 9))
+		if (a / 10 >= 10)
 		{
-			_putchar(n + '0');
+			_putchar(a / 100 + '0');
+			_putchar((a / 10) % 10 + '0');
+			_putchar(a % 10);
 		}
 
-		if (n > 9)
+		if (a / 10 < 10)
 		{
-			_putchar(n / 10 + '0');
-			_putchar(n % 10 + '0');
-		}
-
-		_putchar(',');
-		_putchar(' ');
-		n++;
-	}
-
-	while (n > 98)
-	{
-		if (n / 10 >= 10)
-		{
-			_putchar(n / 100 + '0');
-			_putchar((n / 10 ) % 10 + '0');
-			_putchar(n % 10);
-		} else
-		{
-			_putchar(n / 10 + '0');
-			_putchar(n % 10 + '0');
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
 		}
 
 		_putchar(',');
 		_putchar(' ');
-		n--;
+
+		if (n < 98)
+		{
+			n++;
+		}
+
+		if (n > 98)
+		{
+			n--;
+		}
 	}
 
-	if (n == 98)
-	{
-		_putchar('9');
-		_putchar('8');
-		_putchar('\n');
-	}
+	_putchar('9');
+	_putchar('8');
+	_putchar('\n');
 }
