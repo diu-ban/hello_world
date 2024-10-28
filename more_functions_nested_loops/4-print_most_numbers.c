6b1bfbd464cf89901f82422215d17b57ce7a523d
diu <bongban2003@gmail.com>
2024-10-28 11:24:54 +0000

    mfnl 4 betty fix

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -4,13 +4,13 @@
  * print_most_numbers - Prints numbers from 0 to 9 excluding 2 and 4
  *
  * Description: This function prints the digits from 0 to 9, except
- *              for 2 and 4, followed by a newline. Uses only _putchar for output.
+ *              for 2 and 4, followed by a newline. Uses _putchar for output.
  */
 void print_most_numbers(void)
 {
 	int a = 0;
 
-	while ( a < 10)
+	while (a < 10)
 	{
 		if ((a != 2) && (a != 4))
 		{
