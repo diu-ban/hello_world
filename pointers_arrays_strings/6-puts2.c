18eca3afe66097920cebc6840f99641efb680ab0
diu <bongban2003@gmail.com>
2024-10-29 09:02:32 +0000

    pas 6 fix

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -5,7 +5,7 @@
  * @str: Pointer to the string to be processed.
  *
  * Description: This function iterates through the given string and
- *              prints every second character where the ASCII value of 
+ *              prints every second character where the ASCII value of
  *              the character is even. It ends with a newline.
  */
 void puts2(char *str)
@@ -14,7 +14,7 @@ void puts2(char *str)
 
 	while (str[i] != '\0')
 	{
-		if (str[i] % 2 == 0)
+		if (i % 2 == 0)
 		{
 			_putchar(str[i]);
 		}
