4dd97dbc41c4b410005a8bd5986222a07d347f7c
diu <bongban2003@gmail.com>
2024-10-29 09:17:37 +0000

    pas 8 fix

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -12,16 +12,23 @@
  */
 void print_array(int *a, int n)
 {
-	int i;
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		if(i != (n - 1))
+		int i;
+		for (i = 0; i < n; i++)
 		{
-			printf("%d, ", a[i]);
-		}
-		else
-		{
+			if(i != (n - 1))
+			{
+				printf("%d, ", a[i]);
+			}
+			else
+			{
 			printf("%d\n", a[i]);
+			}
 		}
+	}
+	else
+	{
+		printf("\n");
 	}
 }
