31705974df7d1538f1f8488938e62094eaad733b
diu <bongban2003@gmail.com>
2024-10-30 04:41:14 +0000

    pas 100 fix

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,25 +1,26 @@
 #include "main.h"
 
 /**
- * atoi - Converts a string to an integer.
+ * _atoi - Converts a string to an integer.
  * @s: The string to be converted.
  *
  * Description: The function takes a string containing characters, signs,
- * and numbers. It skips non-numeric characters at the start
- * but takes into account multiple '-' or '+' signs, using
- * the last sign to determine the final result's sign.
- * Conversion stops once non-numeric characters appear after
- * numbers.
+ *              and numbers. It skips non-numeric characters at the start
+ *              but takes into account multiple '-' or '+' signs, using
+ *              the last sign to determine the final result's sign.
+ *              Conversion stops once non-numeric characters appear after
+ *              numbers.
  *
  * Return: The integer value of the first number sequence in the string.
- * Returns 0 if no numbers are found.
+ *         Returns 0 if no numbers are found.
  */
 int _atoi(char *s)
 {
 	int sign = 1;
 	int result = 0;
 	int started = 0;
-    
+	int mul = 0;
+
 	while (*s != '\0')
 	{
 		if ((*s == '-') && (!started))
@@ -33,30 +34,19 @@ int _atoi(char *s)
 		if ((*s >= '0') && (*s <= '9'))
 		{
 			started = 1;
+
 			if (sign == 1)
 			{
-				if (result > 2147483647 / 10 ||
-				(result == 2147483647 / 10 && (*s - '0') > 2147483647 % 10))
-				{
-					return 2147483647;
-				}
-				else
-				{
-					result = result * 10 + (*s - '0');
-				}
+				result = result * 10 + (*s - '0');
+			}
+			else if ((sign == -1) && (!mul))
+			{
+				result = result * 10 + (*s - '0');
+				result *= -1;
 			}
 			else
 			{
-				if (result > 2147483648 / 10 ||
-				(result == 2147483648 / 10 && (*s - '0') > 8))
-				{
-					sign = 1;
-					return -2147483648;
-				}
-				else
-				{
-					result = result * 10 + (*s - '0');
-				}
+				result = result * 10 - (*s - '0');
 			}
 		}
 		else if (started)
@@ -66,12 +56,5 @@ int _atoi(char *s)
 		s++;
 	}
 
-	if (result == -2147483648)
-	{
-		return (result);
-	}
-	else
-	{  
-		return (sign * result);
-	}
+	return result;
 }
