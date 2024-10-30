6fec1bea00e2e342dd90f089d45b4cc02bd64a24
diu <bongban2003@gmail.com>
2024-10-30 05:19:39 +0000

    pas advace 1st try

---
+++ pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int arr[15];
+
+	srand(time(NULL));
+
+	for (int i = 0; i < 15; i++)
+	{
+		a[i] = rand() % 128;
+	}
+
+	for (int y = 0; y < 15; y++)
+	{
+		printf("%c", a[i]);
+	}
+
+	return (0);
+}
