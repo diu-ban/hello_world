96966c3dc98c9daa609f3fd1f3d99ff9bb8d2ee9
diu <bongban2003@gmail.com>
2024-10-30 04:13:32 +0000

    pas 100 fix

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,25 +1,25 @@
 #include "main.h"
 
 /**
- * _atoi - Converts a string to an integer.
+ * atoi - Converts a string to an integer.
  * @s: The string to be converted.
  *
  * Description: The function takes a string containing characters, signs,
- *              and numbers. It skips non-numeric characters at the start
- *              but takes into account multiple '-' or '+' signs, using
- *              the last sign to determine the final result's sign.
- *              Conversion stops once non-numeric characters appear after
- *              numbers.
- * 
+ * and numbers. It skips non-numeric characters at the start
+ * but takes into account multiple '-' or '+' signs, using
+ * the last sign to determine the final result's sign.
+ * Conversion stops once non-numeric characters appear after
+ * numbers.
+ *
  * Return: The integer value of the first number sequence in the string.
- *         Returns 0 if no numbers are found.
+ * Returns 0 if no numbers are found.
  */
-int _atoi(char *s)
+int atoi(char *s)
 {
 	int sign = 1;
 	int result = 0;
 	int started = 0;
-
+    
 	while (*s != '\0')
 	{
 		if ((*s == '-') && (!started))
@@ -30,21 +30,25 @@ int _atoi(char *s)
 		{
 			sign *= 1;
 		}
-
 		if ((*s >= '0') && (*s <= '9'))
 		{
 			started = 1;
-
-			if ((sign == 1) && (result > (2147483647 - (*s - '0')) / 10))
+			if (sign == 1)
 			{
-				return (2147483647);
-			}
-			else if ((sign == -1) && (-result < (-2147483648 + (*s - '0')) / 10))
-			{
-				return (-2147483648);
+				if (result > 2147483647 / 10 ||
+				(result == 2147483647 / 10 && (*s - '0') > 2147483647 % 10))
+				{
+					return 2147483647;
+				}
+				result = result * 10 + (*s - '0');
 			}
 			else
 			{
+				if (result > 2147483648 / 10 ||
+				(result == 2147483648 / 10 && (*s - '0') > 8))
+				{
+					return -2147483648;
+				}
 				result = result * 10 + (*s - '0');
 			}
 		}
@@ -52,7 +56,6 @@ int _atoi(char *s)
 		{
 			break;
 		}
-
 		s++;
 	}
 
