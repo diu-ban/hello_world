44284fd206ce0915be44742825cc5384f6eb4b84
diu <bongban2003@gmail.com>
2024-10-31 09:14:49 +0000

    mpas break

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+char *rot13(char *c)
+{
+	while (*s != '\0')
+	{
+	
+	}
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -23,5 +23,6 @@ char *cap_string(char *s);
 char *leet(char *s);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *rot13(char *);
 
 #endif
