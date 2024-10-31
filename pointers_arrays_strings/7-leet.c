dd37e8e66ee81a57ca73c502f7fc99839e32c0d5
diu <bongban2003@gmail.com>
2024-10-31 04:59:54 +0000

    mpas 7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,6 +1,36 @@
 #include "main.h"
 
+/**
+ * leet - Encodes a string into 1337 (leet) format.
+ * @s: The string to be modified.
+ *
+ * Description: Converts specific letters into leet speak:
+ *              - 'A' or 'a' to '4'
+ *              - 'E' or 'e' to '3'
+ *              - 'O' or 'o' to '0'
+ *              - 'T' or 't' to '7'
+ *              - 'L' or 'l' to '1'
+ *
+ * Return: A pointer to the modified string.
+ */
 char *leet(char *s)
 {
-  
+	char key[] = "AaEeOoTtLl";
+	char val[] = "4433007711";
+	int i;
+	int j;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		for (j = 0; j!= '\0'; j++)
+		{
+			if (s[i] == key[j])
+			{
+				s[i] = val[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
 }
