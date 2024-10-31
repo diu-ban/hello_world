d3554ab368d37c64e1da288b8317af2c0683dc2b
diu <bongban2003@gmail.com>
2024-10-31 07:52:09 +0000

    mpas 4 fix

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -13,14 +13,15 @@ void reverse_array(int *a , int n)
 {
 	while (n > 1)
 	{
-		int i;
+		int i = 0;
 		int temp;
 
-		for (i = 0; i < (n / 2); i++)
+		while (i < n / 2)
 		{
 			temp = a[i];
 			a[i] = a[n - i - 1];
 			a[n - i - 1] = temp;
+			i++;
 		}
 	}
 }
