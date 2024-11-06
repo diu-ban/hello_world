8d8877991d21c9f35619063590b1a6613b5005be
diu <bongban2003@gmail.com>
2024-11-06 10:39:28 +0000

    recursion 100

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -8,12 +8,11 @@
  */
 int palin_len(char *s)
 {
-	int len = 0;
-	while (s[len] != '\0')
+	if (*s == '\0')
 	{
-		len++;
+		return (1);
 	}
-	return (len);
+	return (1 + palin_len(s + 1));
 }
 
 /**
