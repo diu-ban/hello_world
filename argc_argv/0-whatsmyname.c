fd5bf01b2a38a91260cb5bc35c25b55c18538c7a
diu <bongban2003@gmail.com>
2024-11-07 07:22:09 +0000

    acv test

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <strio.h>
+
+/**
+ * main - Prints the name of the program.
+ * @argc: Argument count (number of arguments passed to the program).
+ * @argv: Argument vector (array of argument strings).
+ *
+ * Return: 0 on success.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
