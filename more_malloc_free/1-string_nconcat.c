fb0ec1b32c043d74920533007c4fc98f727e9e6e
diu <bongban2003@gmail.com>
2024-11-11 09:54:33 +0000

    mmf 1 fix

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -42,7 +42,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		size = 1 + i + n;
 	}
 	
-	str = malloc(sizeof(char) * (1 + size));
+	str = malloc(sizeof(char) * size);
 
 	if (str == NULL)
 	{
