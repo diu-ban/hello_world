7a13d815551bd62a786ec261496a6fe6e972f6ed
diu <bongban2003@gmail.com>
2024-11-11 23:01:50 +0000

    st 2 fix betty

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -18,6 +18,6 @@ void print_dog(struct dog *d)
 		return;
 	}
 	printf("Name: %s\n", (d->name) != NULL ? d->name : "(nil)");
-	printf("Age: %7.5f\n", d->age);
+	printf("Age: %7.6f\n", d->age);
 	printf("Owner: %s\n", (d->owner) != NULL ? d->owner : "(nil)");
 }
