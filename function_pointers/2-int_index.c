5dff661ea42429460fbc95beef39b7bf0dab8c67
diu <bongban2003@gmail.com>
2024-11-18 09:36:40 +0000

    fp 3 fix

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -8,11 +8,11 @@
  * @size: The size of the array.
  * @cmp: A pointer to the function used to compare elements of the array.
  *
- * Description: This function iterates through the array `array` of size 
- *              `size` and uses the function pointed to by `cmp` to compare 
- *              each element. If a match is found (i.e., `cmp` returns a 
- *              non-zero value), the index of the first matching element 
- *              is returned. If no match is found, or if the size is less 
+ * Description: This function iterates through the array `array` of size
+ *              `size` and uses the function pointed to by `cmp` to compare
+ *              each element. If a match is found (i.e., `cmp` returns a
+ *              non-zero value), the index of the first matching element
+ *              is returned. If no match is found, or if the size is less
  *              than or equal to zero, -1 is returned.
  *
  * Return: Index of the first element for which `cmp` returns true, or 
