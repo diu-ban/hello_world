18fcfae6c4ca0f4eaca39ed5a3a62d4bafdccdc2
diu <bongban2003@gmail.com>
2024-11-18 09:24:28 +0000

    fp fix

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,6 +1,6 @@
 #include "function_pointers.h"
 #include <stdlib.h>
-
+#include <stddef.h>
 /**
  * array_iterator - Executes a function on each element of an array
  * @array: The array to be processed
